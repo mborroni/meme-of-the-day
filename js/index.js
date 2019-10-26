0492e46d508012f08d4cd7da7e90e05221aad5d4
mborroni <miliborroni@gmail.com>
2019-10-26 13:22:55 -0300

    restructure data memes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 import { getMemes } from "./api.js";
+
+function restructureData(memes) {
+  return memes.map(meme => {
+    const { id, name, width, height, url } = meme;
+    return { id, name, width, height, url };
+  });
+}
+
+function filterBySize(memes) {
+  return memes.filter(meme => meme.width >= 500 || meme.height >= 500);
+}
+
+function orderByAscendingId(memes) {
+  return memes.sort(
+    (memeOne, memeTwo) => parseInt(memeOne.id) - parseInt(memeTwo.id)
+  );
+}
+
+getMemes()
+  .then(restructureData)
+  .then(filterBySize)
+  .then(orderByAscendingId)
+  .then(memes => console.dir(memes))
+  .catch(error => console.log(error.message));
