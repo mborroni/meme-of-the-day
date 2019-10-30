3cc80ddf5ce839cf3444796a6297064a286cfc57
mborroni <miliborroni@gmail.com>
2019-10-30 00:05:46 -0300

    make functions clearer

--- js/api.js
+++ js/api.js
@@ -1,14 +1,17 @@
-const ENDPOINT = "https://api.imgflip.com/get_memes";
+export const ENDPOINT = "https://api.imgflip.com/get_memes";
 
-export function getMemes() {
-  return fetch(ENDPOINT)
-    .then(response => {
-      if (response.ok) {
-        console.log("Successfull request!");
-        return response.json();
-      }
-      console.log(`Oops, we got an error ${response.status}`);
-    })
+function checkStatusCode(response) {
+  if (response.ok) {
+    console.log("Successfull request!");
+  } else {
+    console.log(`Oops, we got an error ${response.status}`);
+  }
+  return response.json();
+}
+
+export function getMemes(URL) {
+  return fetch(URL)
+    .then(checkStatusCode)
     .then(json => json.data.memes)
     .catch(error => console.error(error.message));
 }
