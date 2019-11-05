d26b72ae53e49852a08e5d7109314866ec2158f4
mborroni <miliborroni@gmail.com>
2019-11-05 09:43:29 -0300

    add display of memeOfTheDay and randomMeme

--- js/index.js
+++ js/index.js
@@ -1,15 +1,42 @@
-import { getMemes, ENDPOINT } from "./api.js";
-import { dataRestructuring, bySize, byAscendingId, getToday, getRandomIndex } from "./utils.js";
+import { getMemes, ENDPOINT } from './api.js';
+import {
+  dataRestructuring,
+  bySize,
+  byAscendingId,
+  getToday,
+  getRandomIndex
+} from './utils.js';
 
-function getMemeOfTheDay(memes) {
-  today = getToday();
-  return memes[today - 1];
+let memesArray = [];
+
+const img = document.querySelector('.meme');
+const title = document.querySelector('.title');
+const btn = document.querySelector('.btn-get-random-meme');
+
+btn.addEventListener('click', displayRandomMeme);
+
+function displayMemeOfTheDay() {
+  const memeOfTheDay = getMemeOfTheDay();
+  img.src = memeOfTheDay.url;
+  img.alt = memeOfTheDay.name;
 }
 
-function getRandomMeme(memes) {
-  const randomIndex = getRandomIndex();
-  const randomMeme = memes[randomIndex];
-  return randomMeme;
+function displayRandomMeme() {
+  const randomMeme = getRandomMeme();
+  title.textContent = 'Random Meme';
+  img.src = randomMeme.url;
+  img.alt = randomMeme.name;
+  btn.textContent = 'Get another random Meme!';
+}
+
+function getMemeOfTheDay(memes) {
+  const today = getToday();
+  return memesArray[today - 1];
+}
+
+function getRandomMeme() {
+  const randomIndex = getRandomIndex(memesArray.length);
+  return memesArray[randomIndex];
 }
 
 getMemes(ENDPOINT)
@@ -19,8 +46,9 @@ getMemes(ENDPOINT)
       .filter(bySize)
       .sort(byAscendingId)
   )
-  .then(memes => console.dir(memes))
   .then(memes => {
-    const memeOfTheDay = getMemeOfTheDay(memes);
+    console.dir(memes);
+    memesArray = memes;
+    displayMemeOfTheDay();
   })
   .catch(error => console.log(error.message));
